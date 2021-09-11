26fbd460666a2b770c7179b02ceff94407eb6bc1
mekaala <mekaala@gmail.com>
2021-09-10 20:18:23 -0400

    added rotation and row clearing

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,18 +1,44 @@
 import { useState, useCallback } from 'react';
 import { TETROMINOS, randomTetromino } from '../tetrominos';
-import { STAGE_WIDTH } from '../gameHelpers';
+import { STAGE_WIDTH, checkCollision } from '../gameHelpers';
 
 export const usePlayer = () => {
-    // useState is short for:
-    // const playerState = useState();
-    // const player = playerState[0];
-    // const setPlayer = playerState[1]
-    // useState condenses these lines
     const [player, setPlayer] = useState({
         pos: { x: 0, y: 0 },
         tetromino: TETROMINOS[0].shape,
         collided: false,
     });
+
+    const rotate = (matrix, dir) => {
+        // make rows into columns (transpose)
+        const rotatedTetro = matrix.map((_, index) => 
+            matrix.map(col => col[index]),
+        );
+        // reverse each row to get rotated matrix
+        if (dir > 0) return rotatedTetro.map(row => row.reverse());
+        return rotatedTetro.reverse();
+    };
+
+    const playerRotate = (stage, dir) => {
+        // clone player. Using original player will mutate the state
+        const clonedPlayer = JSON.parse(JSON.stringify(player));
+        clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+        const pos = clonedPlayer.pos.x;
+        let offset = 1;
+        while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+            clonedPlayer.pos.x += offset;
+            offset = -(offset + (offset > 0 ? 1 : -1));
+            if (offset > clonedPlayer.tetromino[0].length) {
+                rotate(clonedPlayer.tetromino, -dir);
+                clonedPlayer.pos.x = pos;
+                return;
+            }
+        }
+
+
+        setPlayer(clonedPlayer);
+    }
 
     const updatePlayerPos = ({ x, y, collided }) => {
         setPlayer(prev => ({
@@ -30,5 +56,5 @@ export const usePlayer = () => {
             });
         },[]);
 
-    return[player, updatePlayerPos, resetPlayer];
+    return[player, updatePlayerPos, resetPlayer, playerRotate];
 };
