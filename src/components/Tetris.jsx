ce291c11aaaf55dff924aef16f6e1581cff66b7b
mekaala <mekaala@gmail.com>
2021-09-10 20:38:47 -0400

    added re-rendering to cells, and added game status hook to Tetris file

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -9,6 +9,7 @@ import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
+import { useGameStatus } from '../hooks/useGameStatus';
 
 // Components
 import Stage from './Stage'
@@ -21,7 +22,8 @@ const Tetris = () => {
 
 
     const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-    const [stage, setStage] = useStage(player, resetPlayer);
+    const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+    const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared);
 
     const movePlayer = dir => {
         if (!checkCollision(player, stage, { x: dir, y: 0 })) {
@@ -35,9 +37,18 @@ const Tetris = () => {
         setDropTime(1000);
         resetPlayer();
         setGameOver(false);
+        setScore(0);
+        setRows(0);
+        setLevel(0);
     }
 
     const drop = () => {
+        // increase level when player has cleared rows
+        if (rows > (level + 1) * 10) {
+            setLevel(prev => prev + 1);
+            // Increase speed
+            setDropTime(1000 / (level + 1) + 200);
+        }
         if (!checkCollision(player, stage, { x: 0, y: 1})) {
             updatePlayerPos({ x: 0, y: 1, collided: false})
         } else {
@@ -54,7 +65,7 @@ const Tetris = () => {
     const keyUp = ({ keyCode }) => {
         if (!gameOver) {
             if (keyCode === 40) {
-                setDropTime(1000);
+                setDropTime(1000 / (level + 1) + 200);
             }
         }
     }
@@ -89,6 +100,7 @@ const Tetris = () => {
 
     return (
         <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)} onKeyUp={keyUp}>
+            <h1>React Tetris</h1>
             <StyledTetris>
                 <Stage stage={stage}/>
                 <aside>
@@ -99,9 +111,9 @@ const Tetris = () => {
                         )
                         : (
                             <div>
-                                <Display text="Score"/>
-                                <Display text="Rows"/>
-                                <Display text="Level"/>
+                                <Display text={`Score: ${score}`}/>
+                                <Display text={`Rows: ${rows}`}/>
+                                <Display text={`Level: ${level + 1}`}/>
                             </div>
                         )
                     }
@@ -112,4 +124,4 @@ const Tetris = () => {
     )
 }
 
-export default Tetris
+export default Tetris;
