bb22ba672dc8d69800fffa4a826e03e0ac07abf4
mekaala <mekaala@gmail.com>
2021-09-13 16:52:16 -0400

    fixed code syntax

--- src/components/styles/StyledStage.jsx
+++ src/components/styles/StyledStage.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const StyledStage =  styled.div`
+export const StyledStage = styled.div`
     display: grid;
     grid-template-rows: repeat(
         ${props => props.height},
