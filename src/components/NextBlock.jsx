436dc5221981b5c01b40d58b84d85f5e75095483
mekaala <mekaala@gmail.com>
2021-09-13 16:52:38 -0400

    began adding preview for next blocks that require redux

---
+++ src/components/NextBlock.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyledNextBlock } from './styles/StyledNextBlock';
+import Cell from './Cell'
+
+const NextBlock = ({ block }) => (
+    <StyledNextBlock width={block[0].length} height={block.length}>
+        {block.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]}/>))}
+    </StyledNextBlock>
+);
+
+export default NextBlock;
