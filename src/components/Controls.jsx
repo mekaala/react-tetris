c005a94973337ba8618fe4d66c1bf256fbb44825
mekaala <mekaala@gmail.com>
2021-09-13 21:31:04 -0400

    added controls component for instructions

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -3,12 +3,13 @@ import { StyledControls, StyledKeys, StyledShift} from './styles/StyledControls'
 
 const Controls = () => (
     <StyledControls>
-        <StyledKeys>→</StyledKeys> Move Right<br/>
+        <h3>Keyboard Controls</h3>
         <StyledKeys>←</StyledKeys> Move Left<br/>
+        <StyledKeys>→</StyledKeys> Move Right<br/>
         <StyledKeys>↓</StyledKeys> Move Down<br/>
-        <StyledKeys>↑</StyledKeys> Rotate Right<br/>
-        <StyledShift>shift</StyledShift> Rotate Left<br/>
-        <StyledKeys>\</StyledKeys> Hard Drop<br/>
+        <StyledKeys>↑</StyledKeys> Hard Drop<br/>
+        <StyledKeys>/</StyledKeys> Rotate Left<br/>
+        <StyledShift>shift</StyledShift> Rotate Right<br/>
     </StyledControls>
 );
 
