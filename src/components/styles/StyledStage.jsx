450b56dc7e9183ff53a77804de9b855dbc267745
mekaala <mekaala@gmail.com>
2021-09-13 21:37:09 -0400

    fixed dimensions of stage :

--- src/components/styles/StyledStage.jsx
+++ src/components/styles/StyledStage.jsx
@@ -4,13 +4,13 @@ export const StyledStage = styled.div`
     display: grid;
     grid-template-rows: repeat(
         ${props => props.height},
-        calc(23vw / ${props => props.width})
+        calc(20vw / ${props => props.width})
     );
     grid-template-columns: repeat(${props => props.width}, 1fr);
     grid-gap: 1px;
     border: 2px solid #333;
     width: 100%;
-    max-width: 23vw;
+    max-width: 20vw;
     background: #002700;
     margin: auto;
 `
